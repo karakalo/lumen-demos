4db517c277fa41ca3c019f2bc126ab0602b27e60
Chip Richards <wmrlum@niestu.com>
2013-01-30 14:56:09 -0700

    Tidied up and added shaders, now works like the original

--- opengl-tutorial/tutorial02.adb
+++ opengl-tutorial/tutorial02.adb
@@ -6,6 +6,7 @@ with Ada.Text_IO;
 
 with Lumen.Events;
 with Lumen.GL;
+with Lumen.Shader;
 with Lumen.Window;
 use Lumen;
 
@@ -13,10 +14,24 @@ procedure Tutorial02 is
 
    ---------------------------------------------------------------------------
 
-   Win              : Lumen.Window.Window_Handle;
-   Terminated       : Boolean := False;
-   Vertex_Array_ID  : GL.UInt;
-   Vertex_Buffer_ID : GL.UInt;
+   use Ada.Text_IO;
+   use type GL.Bitfield;
+
+   ---------------------------------------------------------------------------
+
+   Vertex_Shader_Pathname   : constant String := "data/tutorial02.vertex.shader";
+   Fragment_Shader_Pathname : constant String := "data/tutorial02.fragment.shader";
+
+   ---------------------------------------------------------------------------
+
+   Win                : Lumen.Window.Window_Handle;
+   Terminated         : Boolean := False;
+   Vertex_Array_ID    : GL.UInt;
+   Vertex_Buffer_ID   : GL.UInt;
+   Vertex_Shader_ID   : GL.UInt;
+   Fragment_Shader_ID : GL.UInt;
+   Compiled           : Boolean;
+   Program_ID         : GL.UInt;
 
    ---------------------------------------------------------------------------
 
@@ -43,11 +58,6 @@ procedure Tutorial02 is
 
 begin  -- Tutorial02
 
-   -- Ensure loading of newer (GL 1.5) functions
-   if not Lumen.GL.Load_GL_1_5 then
-      Ada.Text_IO.Put_Line ("Missing OpenGL 1.5 functions");
-   end if;
-
    -- Create Lumen window, accepting most defaults
    Lumen.Window.Create (Win, Name => "Tutorial 02");
    Win.Key_Press := Key_Press'Unrestricted_Access;
@@ -58,24 +68,58 @@ begin  -- Tutorial02
    GL.Gen_Vertex_Arrays (1, Vertex_Array_ID'Address);
    GL.Bind_Vertex_Array (Vertex_Array_ID);
 
-   -- Generate 1 buffer, put the resulting identifier in vertexbuffer
+   -- Create and compile our GLSL program from the shaders
+   Put_Line ("Compiling vertex shader " & Vertex_Shader_Pathname);
+   Shader.From_File (GL.GL_VERTEX_SHADER, Vertex_Shader_Pathname, Vertex_Shader_ID, Compiled);
+   if Compiled then
+      Put_Line ("Vertex shader compiled successfully");
+   else
+      Put_Line ("Vertex shader failed to compile.  Error:");
+      Put_Line (Shader.Get_Info_Log (Vertex_Shader_ID));
+   end if;
+   Put_Line ("Compiling fragment shader " & Fragment_Shader_Pathname);
+   Shader.From_File (GL.GL_FRAGMENT_SHADER, "data/tutorial02.fragment.shader", Fragment_Shader_ID, Compiled);
+   if Compiled then
+      Put_Line ("Fragment shader compiled successfully");
+   else
+      Put_Line ("Fragment shader failed to compile.  Error:");
+      Put_Line (Shader.Get_Info_Log (Fragment_Shader_ID));
+   end if;
+
+   -- Link the program
+   Put_Line ("Linking program");
+   Program_ID := GL.Create_Program;
+   GL.Attach_Shader (Program_ID, Vertex_Shader_ID);
+   GL.Attach_Shader (Program_ID, Fragment_Shader_ID);
+   GL.Link_Program (Program_ID);
+
+   -- Done with these now
+   GL.Delete_Shader (Vertex_Shader_ID);
+   GL.Delete_Shader (Fragment_Shader_ID);
+
+   -- Generate 1 buffer, put the resulting identifier in Vertex_Buffer_ID
    GL.Gen_Buffers (1, Vertex_Buffer_ID'Address);
 
-   -- The following commands will talk about our 'vertexbuffer' buffer
+   -- The following commands will talk about our 'Vertex_Buffer_ID' buffer
    GL.Bind_Buffer (GL.GL_ARRAY_BUFFER, Vertex_Buffer_ID);
 
    -- Give our vertices to OpenGL.
    GL.Buffer_Data (GL.GL_ARRAY_BUFFER, Vertex_Buffer_Data_Size, Vertex_Buffer_Data'Address, GL.GL_STATIC_DRAW);
 
-   -- Loop until user hits the Escape or clicks the window's Close button
+   -- Loop until user hits the Escape (done by the Key_Press routine above) or
+   -- clicks the window's Close button (done by Process_Events)
    while Lumen.Window.Process_Events (Win) loop
 
-      -- Exit when they destroy the window, or when they hit any key
+      -- Exit when they hit the Esc key
       exit when Terminated;
 
       -- Clear the screen
-      GL.Clear (GL.GL_COLOR_BUFFER_BIT);
+      GL.Clear (GL.GL_COLOR_BUFFER_BIT or GL.GL_DEPTH_BUFFER_BIT);
 
+      -- Activate our shaders
+      GL.Use_Program (Program_ID);
+
+      -- The attribute here (0, output of the fragment shader) is the vertex position (X,Y,Z)
       GL.Enable_Vertex_Attrib_Array (0);
       GL.Bind_Buffer (GL.GL_ARRAY_BUFFER, Vertex_Buffer_ID);
       GL.Vertex_Attrib_Pointer
@@ -93,7 +137,8 @@ begin  -- Tutorial02
 
       GL.Disable_Vertex_Attrib_Array (0);
 
-      Lumen.Window.Swap (Win);  -- not necessary since we drew nothing
+      -- Swap in the backbuffer so we can see what we just drew
+      Lumen.Window.Swap (Win);
    end loop;
 
 end Tutorial02;
